b6f10ec67d15fd89788ee15eddd762d1d98749fd
dell <fathi.manai@le-campus-numerique.fr>
2025-08-12 16:37:44 +0200

    commit-test

--- src/Demenagementdynamique.java
+++ src/Demenagementdynamique.java
@@ -5,38 +5,9 @@ public class Demenagementdynamique {
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        int totalCartons = 0;
-        int capaciteCamion = 0;
 
-        while (true) {
-            System.out.print("Entrez le nombre total de cartons à déménager : ");
-            if (clavier.hasNextInt()) {
-                totalCartons = clavier.nextInt();
-                if (totalCartons > 0) {
-                    break;
-                } else {
-                    System.out.println("Veuillez entrer un nombre positif.");
-                }
-            } else {
-                System.out.println("Ce n'est pas un nombre valide.");
-                clavier.next();
-            }
-        }
-
-        while (true) {
-            System.out.print("Entrez la capacité du camion (en nombre de cartons) : ");
-            if (clavier.hasNextInt()) {
-                capaciteCamion = clavier.nextInt();
-                if (capaciteCamion > 0) {
-                    break;
-                } else {
-                    System.out.println("Veuillez entrer un nombre positif.");
-                }
-            } else {
-                System.out.println("Ce n'est pas un nombre valide.");
-                clavier.next();
-            }
-        }
+        int totalCartons = lireEntierPositif(clavier, "Entrez le nombre total de cartons à déménager : ");
+        int capaciteCamion = lireEntierPositif(clavier, "Entrez la capacité du camion (en nombre de cartons) : ");
 
         int cartonsRestants = totalCartons;
         int numeroVoyage = 1;
@@ -50,5 +21,22 @@ public class Demenagementdynamique {
 
         clavier.close();
     }
-}
 
+    public static int lireEntierPositif(Scanner scanner, String message) {
+        int valeur;
+        while (true) {
+            System.out.print(message);
+            if (scanner.hasNextInt()) {
+                valeur = scanner.nextInt();
+                if (valeur > 0) {
+                    return valeur;
+                } else {
+                    System.out.println("Veuillez entrer un nombre positif.");
+                }
+            } else {
+                System.out.println("Ce n'est pas un nombre valide.");
+                scanner.next();
+            }
+        }
+    }
+}
